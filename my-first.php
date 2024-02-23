49ed6aa8277a9c81b5473ba2289592b25c96bae5
Luke Brannock <157344366+labrannock152@users.noreply.github.com>
2024-02-23 16:14:30 -0500

    Add files via upload

--- my-first.php
+++ my-first.php
@@ -4,22 +4,24 @@ File: my-first.php
 Purpose: PHP Practice
 -->
 <html>
-    <head>
-        <title>First PHP Example</title>
-    </head>
-        <body>
-            <h1>Circle Calculation<h1>
+
+<head>
+    <title>First PHP Example</title>
+</head>
+
+<body>
+    <h1>Circle Calculation<h1>
             <?php
             $radius = 15.75;
             $area = pi() * pow($radius, 2);
             $circumference = 2 * pi() * $radius;
 
-            Print("<p>A circle with a radius of $radius has an area of
+            print("<p>A circle with a radius of $radius has an area of
              $area and a circumference of $circumference.</p>");
 
-            print("<p>That'sall that I have been designed to tell
+            print("<p>That's all that I have been designed to tell
              you!</p>");
-             ?>
-        </body>
+            ?>
+</body>
 
 </html>
